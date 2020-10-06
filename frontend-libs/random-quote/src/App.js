7c40011811ec9bd01415980f27138ee4bea8389f
Pratik Var <npc.prat@gmail.com>
2020-10-06 16:26:24 +0530

    First build

---
+++ frontend-libs/random-quote/src/App.js
@@ -0,0 +1,17 @@
+import React, {useState} from 'react';
+import './App.css';
+
+const quotes = {}
+
+// import quotes json
+// create a function which randomly selects a quote from quotes
+// a button which calls that function
+
+
+function App() {
+  return (
+    <div id="quote-box">bruh</div>
+  );
+}
+
+export default App;
