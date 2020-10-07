76c5e523e6c874781ecf40b359bc88bc9d8c55fe
Pratik Var <npc.prat@gmail.com>
2020-10-08 00:18:38 +0530

    Initial commit

---
+++ frontend-libs/js-calculator/src/app.js
@@ -0,0 +1,19 @@
+'use strict';
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        }
+    }
+    render() {
+        return (
+            <div id="calculator">
+
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(<App />, document.querySelector('#root'));
