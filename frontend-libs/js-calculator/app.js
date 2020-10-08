661cb699b45ca08a14ef7979cfdf854f92e5c736
Pratik Var <npc.prat@gmail.com>
2020-10-08 22:20:36 +0530

    Finish project

--- frontend-libs/js-calculator/app.js
+++ frontend-libs/js-calculator/app.js
@@ -8,6 +8,10 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
+var _MaterialUI = MaterialUI,
+    IconButton = _MaterialUI.IconButton,
+    Button = _MaterialUI.Button;
+
 var App = function (_React$Component) {
     _inherits(App, _React$Component);
 
@@ -16,20 +20,208 @@ var App = function (_React$Component) {
 
         var _this = _possibleConstructorReturn(this, (App.__proto__ || Object.getPrototypeOf(App)).call(this, props));
 
-        _this.state = {};
+        _this.state = {
+            initial: true,
+            input: '0',
+            result: '0'
+        };
+        _this.inputHandle = _this.inputHandle.bind(_this);
+        _this.clearCalc = _this.clearCalc.bind(_this);
+        _this.calculateResult = _this.calculateResult.bind(_this);
         return _this;
     }
 
     _createClass(App, [{
+        key: 'inputHandle',
+        value: function inputHandle(value) {
+            var currentInput = this.state.input;
+
+            // update input
+            if (value == '.') {
+                /((?<=[.])\d*[.]*)$/.test(currentInput) ? null : this.setState({
+                    initial: false,
+                    input: currentInput.concat(value)
+                });
+            } else if (this.state.initial) {
+                value == 0 ? null : /[+*/]/g.test(value) ? this.setState({ initial: false, input: '0'.concat(value) }) : this.setState({ initial: false, input: value });
+            } else if (/[+*/]/g.test(value)) {
+                /([+*/\-]\s)$/.test(currentInput) ? this.setState({
+                    input: currentInput.replace(/((\s[+*/]\s)(\s\-\s)?)$/, value)
+                }) : this.setState({ input: currentInput.concat(value) });
+            } else if (value == ' - ') {
+                /(\s-\s)$/.test(currentInput) ? null : this.setState({ input: currentInput.concat(value) });
+            } else {
+                this.setState({
+                    input: currentInput.concat(value)
+                });
+            }
+
+            // update result
+            /[\-.+*/]/g.test(value) ? null : this.state.initial ? this.setState({ result: value }) : this.setState({
+                result: eval(currentInput.concat(value))
+            });
+        }
+    }, {
+        key: 'calculateResult',
+        value: function calculateResult() {
+            this.setState({
+                input: ''.concat(eval(this.state.input)),
+                result: ''.concat(eval(this.state.input))
+            });
+        }
+    }, {
+        key: 'clearCalc',
+        value: function clearCalc() {
+            this.setState({
+                initial: true,
+                input: '0',
+                result: '0'
+            });
+        }
+    }, {
         key: 'render',
         value: function render() {
+            var _this2 = this;
+
             return React.createElement(
                 'div',
                 { id: 'calculator' },
                 React.createElement(
-                    'h1',
-                    null,
-                    'henlo'
+                    'div',
+                    { id: 'displayContainer' },
+                    React.createElement(
+                        'div',
+                        { id: 'display' },
+                        this.state.input
+                    ),
+                    React.createElement(
+                        'div',
+                        { id: 'result' },
+                        this.state.result
+                    )
+                ),
+                React.createElement(
+                    'div',
+                    { id: 'numbers' },
+                    React.createElement(
+                        IconButton,
+                        { onClick: function onClick() {
+                                return _this2.inputHandle('7');
+                            }, id: 'seven' },
+                        '7'
+                    ),
+                    React.createElement(
+                        IconButton,
+                        { onClick: function onClick() {
+                                return _this2.inputHandle('8');
+                            }, id: 'eight' },
+                        '8'
+                    ),
+                    React.createElement(
+                        IconButton,
+                        { onClick: function onClick() {
+                                return _this2.inputHandle('9');
+                            }, id: 'nine' },
+                        '9'
+                    ),
+                    React.createElement(
+                        IconButton,
+                        { onClick: function onClick() {
+                                return _this2.inputHandle('4');
+                            }, id: 'four' },
+                        '4'
+                    ),
+                    React.createElement(
+                        IconButton,
+                        { onClick: function onClick() {
+                                return _this2.inputHandle('5');
+                            }, id: 'five' },
+                        '5'
+                    ),
+                    React.createElement(
+                        IconButton,
+                        { onClick: function onClick() {
+                                return _this2.inputHandle('6');
+                            }, id: 'six' },
+                        '6'
+                    ),
+                    React.createElement(
+                        IconButton,
+                        { onClick: function onClick() {
+                                return _this2.inputHandle('1');
+                            }, id: 'one' },
+                        '1'
+                    ),
+                    React.createElement(
+                        IconButton,
+                        { onClick: function onClick() {
+                                return _this2.inputHandle('2');
+                            }, id: 'two' },
+                        '2'
+                    ),
+                    React.createElement(
+                        IconButton,
+                        { onClick: function onClick() {
+                                return _this2.inputHandle('3');
+                            }, id: 'three' },
+                        '3'
+                    ),
+                    React.createElement(
+                        IconButton,
+                        { onClick: function onClick() {
+                                return _this2.inputHandle('0');
+                            }, id: 'zero' },
+                        '0'
+                    ),
+                    React.createElement(
+                        IconButton,
+                        { onClick: function onClick() {
+                                return _this2.inputHandle('.');
+                            }, id: 'decimal' },
+                        '.'
+                    ),
+                    React.createElement(
+                        IconButton,
+                        { onClick: this.calculateResult, id: 'equals' },
+                        React.createElement('i', { 'class': 'fas fa-equals' })
+                    )
+                ),
+                React.createElement(
+                    'div',
+                    { id: 'operators' },
+                    React.createElement(
+                        Button,
+                        { variant: 'contained', color: 'primary', id: 'clear', onClick: this.clearCalc },
+                        'AC'
+                    ),
+                    React.createElement(
+                        IconButton,
+                        { onClick: function onClick() {
+                                return _this2.inputHandle(' / ');
+                            }, id: 'divide' },
+                        React.createElement('i', { 'class': 'fas fa-divide' })
+                    ),
+                    React.createElement(
+                        IconButton,
+                        { onClick: function onClick() {
+                                return _this2.inputHandle(' * ');
+                            }, id: 'multiply' },
+                        React.createElement('i', { 'class': 'fas fa-times' })
+                    ),
+                    React.createElement(
+                        IconButton,
+                        { onClick: function onClick() {
+                                return _this2.inputHandle(' - ');
+                            }, id: 'subtract' },
+                        React.createElement('i', { 'class': 'fas fa-minus' })
+                    ),
+                    React.createElement(
+                        IconButton,
+                        { onClick: function onClick() {
+                                return _this2.inputHandle(' + ');
+                            }, id: 'add' },
+                        React.createElement('i', { 'class': 'fas fa-plus' })
+                    )
                 )
             );
         }
--- frontend-libs/js-calculator/src/app.js
+++ frontend-libs/js-calculator/src/app.js
@@ -1,16 +1,104 @@
 'use strict';
 
+const { IconButton, Button } = MaterialUI;
+
 class App extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-
+            initial: true,
+            input: '0',
+            result: '0'
         }
+        this.inputHandle = this.inputHandle.bind(this);
+        this.clearCalc = this.clearCalc.bind(this);
+        this.calculateResult = this.calculateResult.bind(this);
     }
+
+    inputHandle(value) {
+        const currentInput = this.state.input;
+
+        // update input
+        if(value == '.') {
+            /((?<=[.])\d*[.]*)$/.test(currentInput)
+            ? null
+            : this.setState({
+                initial: false,
+                input: currentInput.concat(value)
+            });
+        } else if(this.state.initial) {
+            value == 0
+            ? null
+            : /[+*/]/g.test(value)
+              ? this.setState({initial: false, input: '0'.concat(value)})
+              : this.setState({initial: false, input: value});
+        } else if(/[+*/]/g.test(value)) {
+            /([+*/\-]\s)$/.test(currentInput)
+            ? this.setState({
+                input: currentInput.replace(/((\s[+*/]\s)(\s\-\s)?)$/, value)
+              })
+            : this.setState({input: currentInput.concat(value)});
+        } else if(value == ' - ') {
+            /(\s-\s)$/.test(currentInput)
+            ? null
+            : this.setState({input: currentInput.concat(value) });
+        } else {
+            this.setState({
+                input: currentInput.concat(value)
+            });
+        }
+
+        // update result
+        /[\-.+*/]/g.test(value)
+        ? null
+        : this.state.initial
+            ? this.setState({result: value})
+            : this.setState({
+                result: eval(currentInput.concat(value))
+            });
+    }
+    calculateResult() {
+        this.setState({
+            input: ''.concat(eval(this.state.input)),
+            result: ''.concat(eval(this.state.input))
+        });
+    }
+    clearCalc() {
+        this.setState({
+            initial: true,
+            input: '0',
+            result: '0'
+        })
+    }
+
     render() {
         return (
             <div id="calculator">
-                <h1>henlo</h1>
+                <div id="displayContainer">
+                    <div id="display">{this.state.input}</div>
+                    <div id="result">{this.state.result}</div>
+                </div>
+                <div id="numbers">
+                    <IconButton onClick={() => this.inputHandle('7')} id="seven">7</IconButton>
+                    <IconButton onClick={() => this.inputHandle('8')} id="eight">8</IconButton>
+                    <IconButton onClick={() => this.inputHandle('9')} id="nine">9</IconButton>
+                    <IconButton onClick={() => this.inputHandle('4')} id="four">4</IconButton>
+                    <IconButton onClick={() => this.inputHandle('5')} id="five">5</IconButton>
+                    <IconButton onClick={() => this.inputHandle('6')} id="six">6</IconButton>
+                    <IconButton onClick={() => this.inputHandle('1')} id="one">1</IconButton>
+                    <IconButton onClick={() => this.inputHandle('2')} id="two">2</IconButton>
+                    <IconButton onClick={() => this.inputHandle('3')} id="three">3</IconButton>
+                    <IconButton onClick={() => this.inputHandle('0')} id="zero">0</IconButton>
+                    <IconButton onClick={() => this.inputHandle('.')} id="decimal">.</IconButton>
+                    <IconButton onClick={this.calculateResult} id="equals"><i class="fas fa-equals"></i></IconButton>
+                </div>
+                <div id="operators">
+                    <Button variant="contained" color="primary" id="clear" onClick={this.clearCalc}>AC</Button>
+                    <IconButton onClick={() => this.inputHandle(' / ')} id="divide"><i class="fas fa-divide"></i></IconButton>
+                    <IconButton onClick={() => this.inputHandle(' * ')} id="multiply"><i class="fas fa-times"></i></IconButton>
+                    <IconButton onClick={() => this.inputHandle(' - ')} id="subtract"><i class="fas fa-minus"></i></IconButton>
+                    <IconButton onClick={() => this.inputHandle(' + ')} id="add"><i class="fas fa-plus"></i></IconButton>
+                </div>
             </div>
         )
     }
