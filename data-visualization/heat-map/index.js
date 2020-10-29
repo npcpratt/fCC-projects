f03afacc1f389d761af17773373b875ab0337ac8
Pratik Var <npc.prat@gmail.com>
2020-10-29 17:16:21 +0530

    Finish chart

--- data-visualization/heat-map/index.js
+++ data-visualization/heat-map/index.js
@@ -5,51 +5,69 @@ fetch('https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/maste
     console.log(data);
     
     const dataset = data.monthlyVariance;
+
     //heading
     d3.select('.heading')
       .append('h1')
       .text('Monthly Global Land-Surface Temperature')
       .append('p')
-      .text(`${dataset[0].year} - ${dataset[dataset.length - 1].year}: base temperature ${data.baseTemperature}°c`);
+      .text(`${dataset[0].year} - ${dataset[dataset.length - 1].year}: base temperature ${data.baseTemperature}°C`);
     
-    const years = Math.ceil(dataset.length/12);
-    const width = 5*years;
-    const height = 30*12;
+    const numberOfYears = Math.ceil(dataset.length/12);
+    const height = 10*numberOfYears;
+    const width = 80*12;
     const fontSize = 16;
     const padding = {
-        left: 9*fontSize,
-        right: 9*fontSize,
-        top: 1*fontSize,
+        left: 2*fontSize,
+        right: 1*fontSize,
+        top: 7*fontSize,
         bottom: 8*fontSize
     }
-    console.log(width)
+
+    // create main svg and tooltip
     const chart = d3.select('.chart')
                     .append('svg')
                     .attr('width', width + padding.left + padding.right)
                     .attr('height', height + padding.top + padding.bottom);
-    const tooltip = d3.select('.chart').append('div').attr('id', 'tooltip')
-    
-    const yScale = d3.scaleBand().domain([0,1,2,3,4,5,6,7,8,9,10,11])
-                     .rangeRound([0, height], 0, 0);
-    const yAxis = d3.axisLeft()
-                    .scale(yScale)
-                    .tickValues(yScale.domain())
-                    .tickFormat(month => {
-                        let date = new Date(0);
-                        date.setUTCMonth(month);
-                        return d3.time.format.utc('%B')(date);
-                    })
-                    .tickSize(10,1);
+    const tooltip = d3.select('.chart')
+                      .append('div')
+                      .attr('id', 'tooltip')
+                      .style('opacity', 0);
+    // set scales
+    const x = d3.scaleBand()
+                .domain(['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'])
+                .range([padding.left, width + padding.left]);
+    const y = d3.scaleBand()
+                .domain(dataset.map(item => item.year))
+                .range([padding.top, height + padding.top]);
+    const color = (variance) => {
+        let colorScale = d3.scaleLinear()
+                           .domain([d3.min(dataset, d => d.variance), d3.max(dataset, d => d.variance)])
+                           .range([0,1]);
+        return d3.interpolateRdYlGn(colorScale(-variance));
+    }
+
+    // append axes
     chart.append('g')
-         .attr('class', 'y-axis')
-         .attr('id', 'y-axis')
-         .attr('transform', `translate(${padding.left}, ${padding.top})`)
-         .call(yAxis)
-         .append('text')
-         .text('Months')
-         .style('text-anchor', 'middle')
-         .attr('transform', `translate(${-7*fontSize}, ${height/2}) rotate(-90)`);
-    
-    const xScale = d3.scaleOrdinal().domain()
+         .call(d3.axisTop(x))
+         .attr('transform', `translate(0, ${padding.top})`)
+         .call(g => g.select(".domain").remove());
+    chart.append('g')
+         .call(d3.axisLeft(y))
+         .attr('transform', `translate(${padding.left}, 0)`)
+         .call(g => g.select(".domain").remove());
+
+    // append rectangles from the data
+    chart.selectAll('rect')
+         .data(dataset)
+         .join('rect')
+         .attr('width', 79)
+         .attr('height', 9)
+         .attr('x', (d,i) => (d.month-1)*80 + padding.left)
+         .attr('y', (d,i) => (d.year - dataset[0].year)*10 + padding.top)
+         .attr('fill', d => color(d.variance))
+         .on('mouseover', (e,d) => {
+             tooltip.transition().duration(100).style('opacity', 0.9)
+         })
 
 })
