ddbbb916f6f0d104b40786799cd6074a0a57f2e6
Pratik Var <npc.prat@gmail.com>
2020-10-29 19:24:01 +0530

    Add tooltip; finish project

--- data-visualization/heat-map/index.js
+++ data-visualization/heat-map/index.js
@@ -2,18 +2,19 @@ fetch('https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/maste
 .then(res => res.json())
 .then(data => {
     
-    console.log(data);
-    
     const dataset = data.monthlyVariance;
 
     //heading
     d3.select('.heading')
       .append('h1')
+      .attr('id', 'title')
       .text('Monthly Global Land-Surface Temperature')
       .append('p')
+      .attr('id', 'description')
       .text(`${dataset[0].year} - ${dataset[dataset.length - 1].year}: base temperature ${data.baseTemperature}°C`);
     
     const numberOfYears = Math.ceil(dataset.length/12);
+    const monthArray = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
     const height = 10*numberOfYears;
     const width = 80*12;
     const fontSize = 16;
@@ -21,7 +22,7 @@ fetch('https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/maste
         left: 2*fontSize,
         right: 1*fontSize,
         top: 5*fontSize,
-        bottom: 8*fontSize
+        bottom: 6*fontSize
     }
 
     // create main svg and tooltip
@@ -33,34 +34,40 @@ fetch('https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/maste
                       .append('div')
                       .attr('id', 'tooltip')
                       .style('opacity', 0);
+
     // set scales
     const x = d3.scaleBand()
-                .domain(['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'])
+                .domain(monthArray)
                 .range([padding.left, width + padding.left]);
     const y = d3.scaleBand()
                 .domain(dataset.map(item => item.year))
                 .range([padding.top, height + padding.top]);
+    const tempDomain = [
+        d3.max(dataset, d => data.baseTemperature + d.variance),
+        d3.min(dataset, d => data.baseTemperature + d.variance)
+    ]
     const color = (variance) => {
         let colorScale = d3.scaleLinear()
-                           .domain([d3.min(dataset, d => data.baseTemperature - d.variance), d3.max(dataset, d => data.baseTemperature - d.variance)])
+                           .domain(tempDomain)
                            .range([0,1]);
-        return d3.interpolateRdYlGn(colorScale(data.baseTemperature - variance));
+        return d3.interpolateRdYlGn(colorScale(data.baseTemperature + variance));
     }
 
     // append legend and axes
     chart.append("g")
     .append(() => legend({
-      color: d3.scaleSequential([d3.max(dataset, d => data.baseTemperature - d.variance), d3.min(dataset, d => data.baseTemperature - d.variance)], d3.interpolateRdYlGn),
+      color: d3.scaleSequential(tempDomain, d3.interpolateRdYlGn),
       title: "Temperature (°C)"
-    }))
-    .attr('transform', `translate(${padding.left}, ${padding.top})`)
+    }));
 
     chart.append('g')
          .call(d3.axisTop(x))
+         .attr('id', 'x-axis')
          .attr('transform', `translate(0, ${padding.top})`)
          .call(g => g.select(".domain").remove());
     chart.append('g')
          .call(d3.axisLeft(y))
+         .attr('id', 'y-axis')
          .attr('transform', `translate(${padding.left}, 0)`)
          .call(g => g.select(".domain").remove());
 
@@ -68,6 +75,10 @@ fetch('https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/maste
     chart.selectAll('rect')
          .data(dataset)
          .join('rect')
+         .attr('class', 'cell')
+         .attr('data-month', d => d.month-1)
+         .attr('data-year', d => d.year)
+         .attr('data-temp', d => data.baseTemperature + d.variance)
          .attr('width', 79)
          .attr('height', 9)
          .attr('x', (d,i) => (d.month-1)*80 + padding.left)
@@ -75,7 +86,17 @@ fetch('https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/maste
          .attr('fill', d => color(d.variance))
          .on('mouseover', (e,d) => {
              tooltip.transition().duration(100).style('opacity', 0.9);
-
+             tooltip.html(
+                 `${d.year} - ${monthArray[d.month-1]} <br> 
+                 ${Math.round((data.baseTemperature + d.variance)*100) / 100}°C <br> 
+                 ${Math.round(d.variance * 100) / 100}°C`
+                 )
+             tooltip.style('left', (d.month-1)*80 + padding.left + 'px')
+             tooltip.style('top', (d.year - dataset[0].year)*10 + padding.top + 'px')
+             tooltip.attr('data-year', d.year)
          })
+         .on('mouseout', () => {
+            tooltip.transition().duration(100).style('opacity', 0);
+         });
 
 })
