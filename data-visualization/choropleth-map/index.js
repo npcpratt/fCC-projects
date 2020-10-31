636271a37d0b6a9ce13fb88647a5eda6b67733bf
Pratik Var <npc.prat@gmail.com>
2020-10-31 22:38:39 +0530

    Add tooltip; finish project

--- data-visualization/choropleth-map/index.js
+++ data-visualization/choropleth-map/index.js
@@ -3,36 +3,64 @@ Promise.all([
   fetch('https://raw.githubusercontent.com/no-stack-dub-sack/testable-projects-fcc/master/src/data/choropleth_map/counties.json')
 ]).then(responses => Promise.all(responses.map(res => res.json())))
   .then(arr => {
-    const data = Object.assign(new Map(arr[0].map(d => [d.fips, d.bachelorsOrHigher])), {title: "Percentage of people aged 24+ with a bachelor's degree or higher"});
+    const eduData = arr[0];
     const mapData = arr[1];
-    console.log(arr[0]);
-    console.log(arr[1]);
-    console.log(data);
+
+    const min = d3.min(eduData.map(d => d.bachelorsOrHigher));
+    const max = d3.max(eduData.map(d => d.bachelorsOrHigher));
+    const colorScale = d3.scaleLinear().domain([min, max]).range([3, 66])
+    const color = d3.scaleQuantize([3, 66], d3.schemeBlues[7]);
     
+    const data = Object.assign(new Map(eduData.map(d => [d.fips, d.bachelorsOrHigher])));
+    const path = d3.geoPath();
+    const us = mapData;
+
+    // create main svg and tooltip
     const svg = d3.select('.chart')
                   .append('svg')
                   .attr('height', '600')
                   .attr('width', '960')
+    const tooltip = d3.select('.chart')
+                      .append('div')
+                      .attr('class', 'tooltip')
+                      .style('opacity', 0);
 
-    const path = d3.geoPath();
+    // append legend
+    svg.append("g")
+    .attr("transform", "translate(610,20)")
+    .append(() => legend({color, width: 260}));
 
-    const us = mapData;
-    // const color = d3.scaleThreshold()
-    //                 .domain(d3.range(2.6, 75.1, (75.1 - 2.6) / 8))
-    //                 .range(d3.schemeGreens[9]);
-    const color = d3.scaleQuantize([2.6, 75.1, (75.1 - 2.6) / 8], d3.schemeBlues[9])
-    
+    // create map
     svg.append("g")
     .selectAll("path")
     .data(topojson.feature(us, us.objects.counties).features)
     .join("path")
-      .attr("fill", d => color(data.get(d.id)))
-      .attr("d", path);
-
+      .attr("fill", d => color(colorScale(data.get(d.id))))
+      .attr("d", path)
+      .on('mouseover', (e, d) => {
+        tooltip.style('opacity', 0.9);
+        tooltip.html(() => {
+          let check = eduData.filter(item => item.fips === d.id);
+          if(check[0]) return (`${check[0].area_name}, ${check[0].state}: ${check[0].bachelorsOrHigher}%`)
+          else return 0;
+        })
+        tooltip.attr('data-education', () => {
+          let check = eduData.filter(item => item.fips === d.id);
+          if(check[0]) return check[0].bachelorsOrHigher;
+          else return 0;
+        })
+        tooltip.style('left', e.pageX + 10 + 'px')
+        tooltip.style('top', e.pageY - 28 + 'px');
+      })
+      .on('mouseout', () => {
+        tooltip.style('opacity', 0);
+      });
+    
+    // create state borders
     svg.append("path")
     .datum(topojson.mesh(us, us.objects.states, (a, b) => a !== b))
     .attr("fill", "none")
-    .attr("stroke", "#111111")
+    .attr("stroke", "#78909c")
     .attr("stroke-linejoin", "round")
     .attr("d", path);
   })
