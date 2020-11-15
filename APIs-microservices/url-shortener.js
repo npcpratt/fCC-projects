70f48f2b59b6f618a701cd38c88f143f0b8c6294
Pratik Var <npc.prat@gmail.com>
2020-11-16 02:15:52 +0530

    Mongoose complete

--- APIs-microservices/url-shortener.js
+++ APIs-microservices/url-shortener.js
@@ -1,54 +1,46 @@
-const url = 'mongodb+srv://pratvar:<password>@cluster0.eryn5.mongodb.net/<dbname>?retryWrites=true&w=majority';
-mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
-const dns = require('dns');
 const bodyParser = require('body-parser');
+const dns = require('dns');
+
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+const url = 'mongodb+srv://pratvar:<password>@cluster0.eryn5.mongodb.net/urlDB?retryWrites=true&w=majority';
+const connection = mongoose.createConnection(url, { useNewUrlParser: true, useUnifiedTopology: true });
+const autoIncrement = require('mongoose-auto-increment');
+autoIncrement.initialize(connection);
 
 const urlSchema = new Schema({
   url: String
 })
-const UrlModel = mongoose.model('UrlModel', urlSchema);
+urlSchema.plugin(autoIncrement.plugin, 'URL');
+const URL = connection.model('URL', urlSchema);
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.post('/api/shorturl/new', (req, res) => {
   let host = req.body.url.replace(/^(https?:\/\/)?(www.)?/, '');
   console.log(host);
-  dns.lookup(host, 6, (err) => {
+  dns.lookup(host, (err) => {
     console.log(err);
+    // check if valid URL
     if (err || /https?:\/\//.test(req.body.url) == false)
     res.send({error: 'invalid url'});
     else {
-      
-      // check if already exists in database
-      let check = {exists: false, doc: null}
-      const checkFunc = (done) => {
-        UrlModel.find({url: req.body.url}, (err, doc) => {
-          if(err) return done(err);
-          else if(doc == []) done(null, doc)
-          else {
-            existing.docID = doc._id;
-            done(null, doc);
-          }
-        });
-      }
-      checkFunc();
-      if(check.exists) 
-        res.send({
-          original_url: req.body.url, 
-          short_url: check.docID
-        });
-      else {
-        // create new document for UrlObj model
-        const createFunc = (done) => {
-          let urlObj = new UrlModel({url: req.body.url});
-          urlObj.save((err, data) => {
-            if(err) return done(err);
-            console.log(data);
-            done(null, data);
+      // check if URL already exists in database
+      URL.find({url: req.body.url}, (err, docs) => {
+        console.log(docs);
+        if(err) return console.error(err);
+        if(docs !== []) {
+          res.send({original_url: req.body.url, short_url: docs[0]._id});
+        } else {
+        // if not, create new record for URL
+          let urlObj = new URL({url: req.body.url});
+          urlObj.save((err) => {
+            if(err) return console.error(err);
+            URL.nextCount((err, count) => {
+              res.send({original_url: req.body.url, short_url: count});
+            });
           })
         }
-        createFunc();
-        res.send({original_url: req.body.url, short_url: ''});
-      }
+      })
     }
   })
-})
+});
