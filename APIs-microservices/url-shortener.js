a4c87c40dc11140f9d680a2a26c52128ee14e300
Pratik Var <npc.prat@gmail.com>
2020-11-15 16:57:26 +0530

    URL validation

--- APIs-microservices/url-shortener.js
+++ APIs-microservices/url-shortener.js
@@ -1,4 +1,17 @@
 const url = 'mongodb+srv://pratvar:<password>@cluster0.eryn5.mongodb.net/<dbname>?retryWrites=true&w=majority';
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
+const dns = require('dns');
+const bodyParser = require('body-parser');
 
-import dns from 'dns';
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.post('/api/shorturl/new', (req, res) => {
+  let host = req.body.url.replace(/^(https?:\/\/)?(www.)?/, '');
+  console.log(host);
+  dns.lookup(host, 6, (err) => {
+    console.log(err);
+    if (err || /https?:\/\//.test(req.body.url) == false)
+    res.send({error: 'invalid url'});
+    else res.send({original_url: req.body.url, short_url: ''});
+  })
+})
