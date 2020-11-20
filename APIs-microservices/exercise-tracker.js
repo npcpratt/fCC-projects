51e1745490bfc9c98466bf56b9071316849ddd8c
Pratik Var <npc.prat@gmail.com>
2020-11-20 23:54:59 +0530

    Add exercises

---
+++ APIs-microservices/exercise-tracker.js
@@ -0,0 +1,64 @@
+const userSchema = new Schema({
+  username: String,
+});
+const User = mongoose.model('User', userSchema);
+const exerciseSchema = new Schema({
+  username: String,
+  date: Date,
+  duration: Number,
+  description: String
+});
+const Exercise = mongoose.model('Exercise', exerciseSchema);
+
+app.post('/api/exercise/new-user', (req, res) => {
+  User.findOne({username: req.body.username}).exec((err, doc) => {
+    if(err) return console.error(err);
+    if(doc !== null) {
+      res.send('Username already taken');
+    } else {
+      let user = new User({username: req.body.username});
+      user.save(err => {
+        if(err) return console.error(err);
+        res.json({username: user.username, _id: user._id});
+      });
+    }
+  });
+});
+app.get('/api/exercise/users', (req,res) => {
+  User.find({}, (err, docs) => {
+    if(err) return console.error(err);
+    res.json(docs);
+  });
+});
+app.post('/api/exercise/add', (req, res) => {
+  User.findOne({_id: req.body.userId}, (err, doc) => {
+    if (doc !== null) {
+      if(err) return console.error(err);
+      let exercise = new Exercise({
+      userId: req.body.userId,
+      username: doc.username,
+      description: req.body.description,
+      duration: req.body.duration
+      });
+      if(req.body.date) exercise.date = req.body.date;
+      exercise.save(err => {
+        if(err) return console.error(err);
+        if(req.body.date)
+          res.json({
+            _id: req.body.userId,
+            username: doc.username,
+            description: exercise.description,
+            duration: exercise.duration,
+            date: exercise.date
+          });
+        else
+          res.json({
+            _id: exercise.userId,
+            username: doc.username,
+            description: exercise.description,
+            duration: exercise.duration
+          });
+      })
+    } else res.send('User does not exist')
+  });
+});
