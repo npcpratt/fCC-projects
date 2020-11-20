5add8d6b1feb38c62cc40c2373b76490644be24d
Pratik Var <npc.prat@gmail.com>
2020-11-21 02:20:04 +0530

    Fix

--- APIs-microservices/file-metadata-service.js
+++ APIs-microservices/file-metadata-service.js
@@ -1,12 +1,10 @@
 var multer  = require('multer')
 var upload = multer()
 
-app.post('/api/fileanalyse', upload.none(), (req, res) => {
-  app.use('/api/fileanalyse', upload.single('upfile'), (req, res) => {
-    res.json({
-      name: req.file.originalname,
-      type: req.file.mimetype,
-      size: req.file.size
-    })
-  })
-  
+app.use('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  });
+});
